2284c034b96726dad0e18475d12caecb150f45e5
ByeongSu Go <107296751+eclipse1228@users.noreply.github.com>
2023-04-08 10:50:28 +0900

    Create function_overloading.cpp

---
+++ function_overloading.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+
+add (int a,int b){
+  return a+b; 
+}
+
+add(double a,double b){
+  return a+b;
+}
+
+int main()
+{
+  int a=1 , b=2;
+   double c=1.1, d=2.2;
+  
+ cout << add(a,b) << endl;
+ cout << add(c,d) << endl; // double add 라고 안 써도 알아서 c,d가 double이기 때문에 compiler가 찾아서 들어간다.
+  
+  
+}
